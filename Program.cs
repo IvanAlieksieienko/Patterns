e2e54016d98e77942beaf790acb0afbf35c6aa72
Ivan Alieksieienko <ivan.alieksieienko@tesseris.com>
2021-02-15 17:07:49 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Singleton.Example
+{
+    public class Government
+    {
+        private static Government instance;
+
+        private Government()
+        {
+            O.C("Private constructor fired!");
+        }
+
+        public static Government GetInstance()
+        {
+            if (Government.instance == null)
+                Government.instance = new Government();
+            return Government.instance;
+        }
+
+        public void SomeAction()
+        {
+            O.C("Some action from government!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            O.C("Start of the programm!!!");
+            // Government government = new Government();    -> impossible because of private constructor
+            Government government = Government.GetInstance();
+            government.SomeAction();
+        }
+    }
+
+    static class O
+    {
+        public static void C(string something)
+        {
+            Console.WriteLine(something);
+        }
+    }
+}
